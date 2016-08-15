25177762b3d36d082a8cd560a544e52d9738b2ec
Jacob Bednarz <jacob.bednarz@gmail.com>
2016-08-16 08:53:57 +1000

    Add known unactionable "block-uri"
    This is a list of the blocked URIs that are outside of your control and
    is best to ignore otherwise you waste your days trying to fix them.

--- jobs/create_csp_violation_job.rb
+++ jobs/create_csp_violation_job.rb
@@ -3,6 +3,31 @@ require 'json'
 class CreateCspViolationJob
   @queue = :create_csp_violations
 
+  KNOWN_UNACTIONABLE_REPORT_URIS = %w(
+    resource://
+    chromenull://
+    chrome-extension://
+    safari-extension://
+    mxjscall://
+    webviewprogressproxy://
+    res://
+    mx://
+    safari-resource://
+    chromenull://
+    chromeinvoke://
+    chromeinvokeimmediate://
+    mbinit://
+    opera://
+    localhost
+    127.0.0.1
+    none://
+    chromenull://
+    resource://
+    about:blank
+  )
+
+  attr_accessor :violation_report
+
   def self.perform(enforced, violation)
     puts "Got #{enforced} and #{violation}"
   end
